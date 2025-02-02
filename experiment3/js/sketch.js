b2b54586f64886e4fae8fe0366f03524ff8769f4
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2025-02-01 17:51:22 -0800

    Added drawing with finger.

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -5,11 +5,16 @@
 // IDEAS:
 //Game where you unlock things by making hand gestures
 //Drawing program where you can draw with your hands
+//Draw with your fingers. Pinch to not draw?
 
 let handPose;
 let video;
 let hands = [];
 let isDetecting = true;
+let isDrawing = false;
+let artSize = 20;
+let savedPoints = [];
+let artColor = [255, 0, 0];
 
 // Globals
 let myInstance;
@@ -53,21 +58,50 @@ function setup() {
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
   background(220);    
-  // Draw the webcam video
-  image(video, 0, 0, width, height);
+  //move image by the width of image to the left
+  translate(video.width, 0);
+  //then flip the image
+  scale(-1, 1);
+  //draw video capture feed as image inside p5 canvas
+  image(video, 0, 0, video.width, video.height);
+  drawPoints();
+  randomizeDrawing();
 
   // Draw all the tracked hand points
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
-    for (let j = 0; j < hand.keypoints.length; j++) {
-      let keypoint = hand.keypoints[j];
-      fill(0, 255, 0);
-      noStroke();
-      circle(keypoint.x, keypoint.y, 10);
+    detectHandPosition(hand);
+    if (isDetecting){
+      for (let j = 0; j < hand.keypoints.length; j++) {
+        let keypoint = hand.keypoints[j];
+        fill(0, 255, 0);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 10);
+      }
+      if (isDrawing) savedPoints.push([hands[i].keypoints[8].x, hands[i].keypoints[8].y]);
     }
   }
 }
 
+function randomizeDrawing() {
+  //Pick a random number 0-2
+  let randomNum = Math.floor(Math.random() * 3);
+  artColor[randomNum] += random(-15, 15);
+  artSize += random(-0.5, 0.5);
+  if (artSize < 10) { artSize = 10; }
+}
+
+
+
+
+function detectHandPosition(hand) {
+  if (hand.keypoints[8].y > hand.keypoints[7].y){
+    isDrawing = false;
+  }
+  else if (!isDrawing && hand.keypoints[8].y < hand.keypoints[7].y){
+    isDrawing = true;
+  }
+}
 
 // Callback function for when handPose outputs data
 function gotHands(results) {
@@ -87,5 +121,20 @@ function toggleDetection() {
   } else {
     handPose.detectStart(video, gotHands);
     isDetecting = true;
+  }
+}
+
+function drawPoints() {
+  for (let i = 0; i < savedPoints.length; i++) {
+    fill(artColor);
+    noStroke();
+    circle(savedPoints[i][0], savedPoints[i][1], artSize);
+  }
+}
+
+
+function pushSavedPoints(hand) {
+  for (let i = 0; i < hand.keypoints.length; i++) {
+    savedPoints.push([hand.keypoints[i].x, hand.keypoints[i].y]);
   }
 }
