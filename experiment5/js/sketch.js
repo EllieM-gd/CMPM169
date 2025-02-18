6a1e8ddeb28a2ad145a0e0f46189b781ae00324b
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2025-02-17 19:00:33 -0800

    Clean up to visual aspect, added restart, added noise

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -8,13 +8,14 @@
 
 let arr_i = 0;
 let arr = nounArray;
-let SLOW_DELAY = 400;
-let FAST_DELAY = 100;
+let SLOW_DELAY = 300;
+let FAST_DELAY = 150;
 let DELAY_INCREMENT = 50;
 let delay = SLOW_DELAY;
 let nextTime;
 let spinState = 0;
 let programState = 0;
+let sound;
 // 0 is stop, 1 is play, 2 is to-stop
 
 //We need to have the array of the madlib assigned to a variable, and a empty one that we fill so we can print what we already have
@@ -46,6 +47,7 @@ function setup() {
   currentText = getARandomMadLib()
   console.log(currentText)
   nextTime = millis() + delay;
+  sound = loadSound("src/spin.mp3");
   $(window).resize(function() {
     resizeScreen();
   });
@@ -53,9 +55,10 @@ function setup() {
 }
 
 
-
+let tick = 0;
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
+  tick += 1;
   background(32);
   if (millis() > nextTime && spinState != 0) {
     if (spinState == 1) {
@@ -70,6 +73,8 @@ function draw() {
         programState = 0;
       }
     }
+    //Sound Effect by freesound_community from Pixabay
+    sound.play();
     arr_i++; // the index of next pattern
     arr_i %= arr.length; // the index goes to front when it reaches the end https://processing.org/reference/modulo.html
     nextTime = millis() + delay;
@@ -83,31 +88,44 @@ function draw() {
       textIndex++
     }
     arr = getNeededArray(keyWord)
+    //randomize array
+    arr = arr.sort(() => Math.random() - 0.5);
     textIndex++
     programState = 1;
     spinState = 1;
   }
-  else if (programState == 0) {
+  else if (programState == 0 && tick % 10 == 0){ 
     currentTextArray.push(currentText[textIndex])
     textIndex++
+    tick = 0;
+  }
+  if (textIndex >= currentText.length && programState == 0){
+    programState = 3;
   }
 
   textAlign(CENTER, CENTER);
   textSize(32);
   noStroke()
-  fill("rgb(231,22,22)")
+  fill("rgb(72, 203, 243)")
 
   if (spinState != 0){
     // current pattern
-    text(arr[arr_i], width / 2, height / 2);
+    text(arr[arr_i], width / 2, height / 1.5);
 
+    fill("rgb(240, 165, 165)")
     // previous pattern
     let prev_i = (arr_i + arr.length - 1) % arr.length;
-    text(arr[prev_i], width / 2, height / 2 - 30);
+    text(arr[prev_i], width / 2, height / 1.5 - 30);
 
     // next pattern
     let next_i = (arr_i + 1) % arr.length;
-    text(arr[next_i], width / 2, height / 2 + 30);
+    text(arr[next_i], width / 2, height / 1.5 + 30);
+
+    // draw a message to user
+    if (spinState == 1 && millis() % 1000 < 500) {
+      fill("rgb(250, 253, 84)");
+      text("Press the mouse to stop", width / 2, height - 100);
+    }
   }
 
   strokeWeight(1);
@@ -122,12 +140,13 @@ function pushChosenWordToArray(array, index) {
 
 
 function printCurrentArray() {
+  fill("rgb(245, 26, 26)");
   x = 100;
   y = 100;
   for (let i = 0; i < currentTextArray.length; i++) {
     text(currentTextArray[i], x, y);
-    x += 30;
-    if (x > canvasContainer.width() - 100 && currentTextArray[i] == " ") {
+    x += 25;
+    if (x > canvasContainer.width() - 175 && currentTextArray[i] == " ") {
       x = 100;
       y += 50;
     }
@@ -146,5 +165,11 @@ function mousePressed() {
     delay = SLOW_DELAY;
     nextTime = millis() + delay;
     spinState = 1; // play
+  } else if (programState == 3){
+    currentTextArray = [];
+    currentText = getARandomMadLib();
+    textIndex = 0;
+    programState = 0;
+    spinState = 0;
   }
 }
